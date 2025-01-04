ac563dc4f3fa7c5e9f20544938373c7eb067941b
a.baghernejad <alibaghernezhad@gmail.com>
2025-01-04 15:34:07 +0330

    Add initial configuration files for application setup

---
+++ src/Retraven.AppHost/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open Aspire.Hosting
+open Retraven.AppHost.Config
+
+let args = Environment.GetCommandLineArgs() 
+let builder = DistributedApplication.CreateBuilder(args)
+let cache = builder.AddRedis("cache")
+
+let postgres = builder.AddPostgresIfRequired()
+           
+let minIo = builder.AddMinio()
+let rabbitMq = builder.AddRabbitMqIfRequired()
+                .RunWithStableNodeName()
+
+let qdrant = builder.AddQdrant()
+let ollama = builder.AddOllama()
+
+// let ragProject =builder.AddProject<Projects.
+// let apiService = builder.AddProject<Projects.Retraven_WebApi>("webapi", launchProfileName= "webapilaunchprofile")
+//                     .WithReference(postgres)
+//                     .WaitFor(postgres)
+//                     .WithReference(minIo)
+//                     .WaitFor(minIo)
+//                     .WithReference(rabbitMq)
+//                     .WaitFor(rabbitMq)
+//                     .WithReference(qdrant)
+//                     .WaitFor(qdrant)
+//                     .WithHttpHealthCheck("/health")
+                    
+let host = builder.Build()
+host.Run()
